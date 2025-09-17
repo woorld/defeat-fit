670ea27fb80f8b53d25a0b4b2810e70e84a294f6
woorld <17719812+woorld@users.noreply.github.com>
2025-09-17 23:34:06 +0900

    ダイアログのベースコンポーネントを作成

---
+++ src/components/BaseDialog.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const model = defineModel<boolean>({ required: true });
+</script>
+
+<template>
+  <VDialog v-model="model">
+    <VSheet class="pa-8 text-center">
+      <slot />
+    </VSheet>
+    <VBtn
+      class="position-absolute top-0 right-0 mt-2 mr-2 elevation-0"
+      icon="mdi-close"
+      @click="model = false"
+    />
+  </VDialog>
+</template>
