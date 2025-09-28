3c1d3f2178113b7c22f5f7e06ee5986107877b9e
woorld <17719812+woorld@users.noreply.github.com>
2025-09-28 21:44:21 +0900

    統計画面 スタイル・レイアウト調整

--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -15,23 +15,24 @@ const stats = ref<Stats[]>([]);
     <ViewHeading title="統計" />
     <VCard
       v-for="statsItem in stats"
+      class="mb-6"
+      :title="statsItem.date"
     >
-      <template #title>
-        <span>{{ statsItem.date }}</span>
-        <span class="ml-4 d-inline-flex justify-center align-center text-grey">
-          <VIcon size="20">mdi-coffin</VIcon>
-          {{ statsItem.defeatCount }}
-        </span>
-      </template>
       <template #text>
-        <VTable density="compact">
-          <tbody>
-            <tr v-for="menuItem of statsItem.menu">
-              <td>{{ menuItem.name }}</td>
-              <td>{{ menuItem.count }} {{ menuItem.unit }}</td>
-            </tr>
-          </tbody>
-        </VTable>
+        <div class="d-flex justify-space-between align-center ga-4">
+          <span class="text-h4 w-25">
+            <VIcon>mdi-coffin</VIcon>
+            {{ statsItem.defeatCount }}
+          </span>
+          <VTable class="flex-1-1-0" density="compact">
+            <tbody>
+              <tr v-for="menuItem of statsItem.menu">
+                <td>{{ menuItem.name }}</td>
+                <td class="text-right">{{ menuItem.count }} {{ menuItem.unit }}</td>
+              </tr>
+            </tbody>
+          </VTable>
+        </div>
       </template>
     </VCard>
   </VContainer>
