7d1dd8759932d43b807263dd5ef6b3f27e1ad4a6
woorld <17719812+woorld@users.noreply.github.com>
2025-09-28 21:28:44 +0900

    統計画面 仮実装

--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -1,8 +1,38 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import type { Stats } from '../../common/types';
+import ViewHeading from '../components/ViewHeading.vue';
+
+const stats = ref<Stats[]>([]);
+
+(async () => {
+  stats.value = await window.stats.getStats();
+})();
 </script>
 
 <template>
   <VContainer>
-    StatsView
+    <ViewHeading title="統計" />
+    <VCard
+      v-for="statsItem in stats"
+    >
+      <template #title>
+        <span>{{ statsItem.date }}</span>
+        <span class="ml-4 d-inline-flex justify-center align-center text-grey">
+          <VIcon size="20">mdi-coffin</VIcon>
+          {{ statsItem.defeatCount }}
+        </span>
+      </template>
+      <template #text>
+        <VTable density="compact">
+          <tbody>
+            <tr v-for="menuItem of statsItem.menu">
+              <td>{{ menuItem.name }}</td>
+              <td>{{ menuItem.count }} {{ menuItem.unit }}</td>
+            </tr>
+          </tbody>
+        </VTable>
+      </template>
+    </VCard>
   </VContainer>
 </template>
