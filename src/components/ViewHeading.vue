0f48dd0758badd759b0ff1810b0f05d0e697995b
woorld <17719812+woorld@users.noreply.github.com>
2025-09-28 21:27:32 +0900

    画面の見出しをコンポーネント化

---
+++ src/components/ViewHeading.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string,
+}>();
+</script>
+
+<template>
+  <h2 class="text-h4 text-center mt-6 mb-10">{{ props.title }}</h2>
+</template>
