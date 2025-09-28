82addbce1cae5d63fbed27cd8d01180bb1db1273
woorld <17719812+woorld@users.noreply.github.com>
2025-09-29 01:52:16 +0900

    統計画面 カードのレイアウトを変更

--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -13,17 +13,22 @@ const stats = ref<Stats[]>([]);
 <template>
   <VContainer>
     <ViewHeading title="統計" />
-    <VCard
-      v-for="statsItem in stats"
-      class="mb-6"
-      :title="statsItem.date"
-    >
+    <VCard v-for="statsItem in stats" class="mb-6">
       <template #text>
         <div class="d-flex justify-space-between align-center ga-4">
-          <span class="text-h4 w-25">
-            <VIcon>mdi-coffin</VIcon>
-            {{ statsItem.defeatCount }}
-          </span>
+          <div class="w-25 text-center">
+            <span class="text-h6">{{ statsItem.date.replace(/\-/g, '/') }}</span>
+            <div class="text-h4 d-flex justify-center align-center mt-3">
+              <VIcon>mdi-coffin</VIcon>
+              <!-- NOTE: marginはアイコンの余白に合わせるためのもの -->
+              <div class="mx-2 mb-1">{{ statsItem.defeatCount }}</div>
+            </div>
+          </div>
+          <VDivider
+            class="mr-3"
+            vertical
+            inset
+          />
           <VTable class="flex-1-1-0" density="compact">
             <tbody>
               <tr v-for="menuItem of statsItem.menu">
