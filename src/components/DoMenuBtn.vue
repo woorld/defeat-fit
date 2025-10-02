d730fb2c7b7cb3da8df12253a39ed7f31dde19bc
woorld <17719812+woorld@users.noreply.github.com>
2025-10-02 20:57:37 +0900

    やるボタンのpropsをMenuに変更

--- src/components/DoMenuBtn.vue
+++ src/components/DoMenuBtn.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 import BaseDialog from './BaseDialog.vue';
-import type { MenuUnit } from '../../common/types';
+import type { Menu } from '../../common/types';
+import { useDefeatCountStore } from '../stores/defeat-count';
+
+const defeatCount = useDefeatCountStore();
 
 const props = defineProps<{
-  defeatCount: number,
-  menuUnit: MenuUnit,
+  menu: Menu,
 }>();
 
-const isShow = ref(false);
+const isVisibleDialog = ref(false);
 const setCount = ref(4);
 
-const secondsPerSet = computed(() => Math.ceil(props.defeatCount / setCount.value));
+const secondsPerSet = computed(() => Math.ceil((defeatCount.count * props.menu.multiplier) / setCount.value));
 const maxSet = computed(() => secondsPerSet.value === 1
   ? setCount.value // 1回あたり1秒になる場合、それ以上セット数を増やしても意味がないため+を押させない
   : Infinity
@@ -20,12 +22,12 @@ const maxSet = computed(() => secondsPerSet.value === 1
 
 <template>
   <VBtn
-    :disabled="defeatCount <= 0"
+    :disabled="defeatCount.count <= 0"
     append-icon="mdi-chevron-right"
     color="green"
   >
     やる
-    <BaseDialog v-model="isShow" activateByParent>
+    <BaseDialog v-model="isVisibleDialog" activateByParent>
       <div class="d-flex justify-center align-center ga-6 flex-column">
         <h3 class="text-h5">何セットに分ける？</h3>
         <div class="d-flex justify-center align-center ga-4">
@@ -41,12 +43,12 @@ const maxSet = computed(() => secondsPerSet.value === 1
           <span class="d-flex justify-center align-baseline ga-2">
             <span class="text-h6">1セット</span>
             <span class="text-h4">{{ secondsPerSet }}</span>
-            <span class="text-h6">{{ props.menuUnit }}</span>
+            <span class="text-h6">{{ props.menu.unit }}</span>
           </span>
         </div>
         <small class="text-grey">※小数繰り上げ</small>
         <VBtn
-          v-show="props.menuUnit === '秒'"
+          v-show="props.menu.unit === '秒'"
           append-icon="mdi-chevron-right"
           color="green"
           :to="`/timer/${secondsPerSet}/${setCount}`"
