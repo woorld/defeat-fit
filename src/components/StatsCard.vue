eba875ea86e0e90138baa70947d47506e7a38e33
woorld <17719812+woorld@users.noreply.github.com>
2025-10-12 23:25:31 +0900

    統計画面 カードのコンポーネント化

---
+++ src/components/StatsCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { Stats } from '../../common/types';
+
+const props = defineProps<{
+  stats: Stats,
+}>();
+</script>
+
+<template>
+  <VCard class="mb-6">
+    <template #text>
+      <div class="d-flex justify-space-between align-center ga-4">
+        <div class="w-25 text-center">
+          <span class="text-h6">{{ props.stats.date.replace(/\-/g, '/') }}</span>
+          <div class="text-h4 d-flex justify-center align-center mt-3">
+            <VIcon>mdi-coffin</VIcon>
+            <!-- NOTE: marginはアイコンの余白に合わせるためのもの -->
+            <div class="mx-2 mb-1">{{ props.stats.defeatCount }}</div>
+          </div>
+        </div>
+        <VDivider
+          class="mr-3"
+          vertical
+          inset
+        />
+        <VTable class="flex-1-1-0" density="compact">
+          <tbody>
+            <tr v-for="menu of props.stats.menuList">
+              <td>{{ menu.name }}</td>
+              <td class="text-right">{{ menu.count }} {{ menu.unit }}</td>
+            </tr>
+          </tbody>
+        </VTable>
+      </div>
+    </template>
+  </VCard>
+</template>
--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -2,6 +2,8 @@
 import { ref, computed } from 'vue';
 import type { StatsMap } from '../../common/types';
 import ViewHeading from '../components/ViewHeading.vue';
+import StatsCard from '../components/StatsCard.vue';
+
 
 const statsMap = ref<StatsMap>(new Map());
 
@@ -25,32 +27,6 @@ const dateDescStatsList = computed(() => {
 <template>
   <VContainer>
     <ViewHeading title="統計" />
-    <VCard v-for="stats in dateDescStatsList" class="mb-6">
-      <template #text>
-        <div class="d-flex justify-space-between align-center ga-4">
-          <div class="w-25 text-center">
-            <span class="text-h6">{{ stats.date.replace(/\-/g, '/') }}</span>
-            <div class="text-h4 d-flex justify-center align-center mt-3">
-              <VIcon>mdi-coffin</VIcon>
-              <!-- NOTE: marginはアイコンの余白に合わせるためのもの -->
-              <div class="mx-2 mb-1">{{ stats.defeatCount }}</div>
-            </div>
-          </div>
-          <VDivider
-            class="mr-3"
-            vertical
-            inset
-          />
-          <VTable class="flex-1-1-0" density="compact">
-            <tbody>
-              <tr v-for="menu of stats.menuList">
-                <td>{{ menu.name }}</td>
-                <td class="text-right">{{ menu.count }} {{ menu.unit }}</td>
-              </tr>
-            </tbody>
-          </VTable>
-        </div>
-      </template>
-    </VCard>
+    <StatsCard v-for="stats in dateDescStatsList" :stats />
   </VContainer>
 </template>
