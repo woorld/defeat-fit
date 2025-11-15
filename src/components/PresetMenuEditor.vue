960410bc456ce47750066183d173f1c1016fa846
woorld <17719812+woorld@users.noreply.github.com>
2025-11-15 19:35:09 +0900

    プリセット編集画面 UI処理、更新処理実装

---
+++ src/components/PresetMenuEditor.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import type { Menu, MenuUnit } from '../../prisma/generated/client';
+import type { SortableEvent } from 'sortablejs';
+import Draggable from 'vuedraggable';
+import { menuUnitMap } from '../../common/util';
+
+const menuList = defineModel<Menu[]>('menuList', { required: true });
+const menuListInPreset = defineModel<Menu[]>('menuListInPreset', { required: true });
+const menuMultiplierList = defineModel<number[]>('menuMultiplierList', { required: true });
+
+const onAddMenu = (event: SortableEvent) => {
+  if (event.newIndex === undefined) {
+    return;
+  }
+  menuMultiplierList.value.splice(event.newIndex, 0, 1);
+};
+
+const onUpdateMenuOrder = (event: SortableEvent) => {
+  if (event.newIndex === undefined || event.oldIndex === undefined) {
+    return;
+  }
+  // 移動したメニューの倍率を対応する位置に移動
+  const targetMultiplier = menuMultiplierList.value.splice(event.oldIndex, 1)[0];
+  menuMultiplierList.value.splice(event.newIndex, 0, targetMultiplier)
+};
+
+const onRemoveMenu = (event: SortableEvent) => {
+  if (event.oldIndex === undefined) {
+    return;
+  }
+  menuMultiplierList.value.splice(event.oldIndex, 1);
+};
+</script>
+
+<template>
+    <VRow>
+      <VCol cols="6">
+        <VSheet class="h-100 rounded">
+          <Draggable
+            v-model="menuList"
+            item-key="key"
+            animation="300"
+            group="menu"
+            class="h-100 overflow-hidden"
+          >
+            <template #item="{ element: menu }">
+              <VCard :title="menu.name" class="ma-2" elevation="4" prepend-icon="mdi-drag" density="compact" />
+            </template>
+          </Draggable>
+        </VSheet>
+      </VCol>
+      <VCol cols="6">
+        <VSheet class="h-100 rounded">
+          <draggable
+            v-model="menuListInPreset"
+            item-key="id"
+            animation="300"
+            group="menu"
+            class="h-100 overflow-hidden"
+            @add="onAddMenu"
+            @update="onUpdateMenuOrder"
+            @remove="onRemoveMenu"
+          >
+            <template #item="{ element: menu, index }">
+              <VCard :title="menu.name" class="ma-2" color="green" prepend-icon="mdi-drag" density="compact">
+                <template #text>
+                  <div class="d-flex justify-center align-center">
+                    <VNumberInput
+                      v-model="menuMultiplierList[index]"
+                      class="mr-4"
+                      hide-details
+                      variant="outlined"
+                      density="compact"
+                      control-variant="stacked"
+                      :precision="1"
+                      :step="0.5"
+                      :min="0.5"
+                    />
+                    <!-- NOTE: menu: Menu & { multiplier?: number } なので型アサーションしてOK -->
+                    {{ menuUnitMap[menu.unit as MenuUnit] }}
+                  </div>
+                </template>
+              </VCard>
+            </template>
+          </draggable>
+        </VSheet>
+      </VCol>
+    </VRow>
+</template>
--- src/views/MenuPresetView.vue
+++ src/views/MenuPresetView.vue
@@ -1,6 +1,113 @@
 <script setup lang="ts">
+// TODO: プリセットがない場合の表示と処理
+// TODO: 空のプリセットを許容しないようにする
+import { ref, computed } from 'vue';
+import { PresetWithMenus } from '../../common/types';
+import type { Menu } from '../../prisma/generated/client';
+import PresetMenuEditor from '../components/PresetMenuEditor.vue';
+
+const presetName = ref('');
+const presetList = ref<PresetWithMenus[]>([]);
+const editingPresetId = ref(0);
+
+const menuList = ref<Menu[]>([]);
+const menuListInPreset = ref<Menu[]>([]);
+const menuMultiplierList = ref<number[]>([]);
+
+const presetSelect = computed(() => presetList.value.map(preset => ({
+  title: preset.name,
+  value: preset.id,
+})));
+
+const getPresetList = async () => {
+  presetList.value = await window.preset.getPresetList();
+};
+
+const selectPreset = async (id: number) => {
+  const preset = presetList.value.find(preset => preset.id === id);
+  if (preset === undefined) {
+    return;
+  }
+
+  presetName.value = preset.name;
+  editingPresetId.value = preset.id;
+  menuListInPreset.value = preset.presetMenuList.map(presetMenu => presetMenu.menu);
+  menuMultiplierList.value = preset.presetMenuList.map(presetMenu => presetMenu.multiplier);
+
+  const fetchedMenuList = await window.menuList.getMenuList();
+  menuList.value = fetchedMenuList.filter(menu => !menuListInPreset.value.map(menu => menu.id).includes(menu.id));
+};
+
+const onSave = async () => {
+  const menuIdWithMultiplierList = menuListInPreset.value.map((menu, index) => ({
+    menuId: menu.id,
+    multiplier: menuMultiplierList.value[index],
+  }));
+
+  if (editingPresetId.value === 0) {
+    // プリセット新規登録
+    const preset = await window.preset.addPreset(
+      presetName.value,
+      menuIdWithMultiplierList
+    );
+    editingPresetId.value = preset.id;
+  }
+  else {
+    // プリセット更新
+    const preset = {
+      id: editingPresetId.value,
+      name: presetName.value,
+    };
+    await window.preset.updatePreset(preset, menuIdWithMultiplierList);
+  }
+
+  getPresetList();
+};
+
+(async () => {
+  await getPresetList();
+  if (presetList.value.length >= 1) {
+    selectPreset(presetList.value[0].id);
+    return;
+  }
+  menuList.value = await window.menuList.getMenuList();
+})();
 </script>
 
 <template>
-  <div>Preset View</div>
+  <div>
+    <div class="d-flex justify-center align-center mb-8">
+      <VSelect
+        v-if="editingPresetId !== 0"
+        v-model="editingPresetId"
+        class="mr-4"
+        hide-details
+        variant="solo"
+        density="compact"
+        rounded
+        :items="presetSelect"
+        @update:modelValue="v => selectPreset(v)"
+      />
+      <VBtn
+        rounded
+        prepend-icon="mdi-plus"
+      >プリセットを追加</VBtn>
+    </div>
+    <div class="d-flex justify-center align-center ga-2 mb-4">
+      <VTextField hide-details placeholder="プリセット名" v-model="presetName" />
+      <VBtn
+        icon="mdi-floppy"
+        color="green"
+        :disabled="menuListInPreset.length <= 0"
+        @click="onSave"
+      />
+      <!-- TODO: 削除処理実装 -->
+      <VBtn icon="mdi-trash-can" color="red" />
+    </div>
+    <PresetMenuEditor
+      :menuList
+      :menuListInPreset
+      :menuMultiplierList
+    />
+  </div>
 </template>
