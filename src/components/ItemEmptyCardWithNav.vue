cece937ca3ac3aef6f9b9762e5fd215ed2efb202
woorld <17719812+woorld@users.noreply.github.com>
2025-11-20 21:17:35 +0900

    ホーム画面のプリセットなし表示を共通化

---
+++ src/components/ItemEmptyCardWithNav.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string,
+  text: string,
+  showPresetNav?: boolean,
+}>();
+</script>
+
+<template>
+  <VCard>
+    <div class="text-center py-2">
+      <VCardTitle>{{ props.title }}</VCardTitle>
+      <VCardText>{{ props.text }}</VCardText>
+    </div>
+    <VDivider />
+    <VList lines="two">
+      <VListItem
+        title="メニュー"
+        subtitle="行う種目（腕立て伏せ、プランクなど）"
+      >
+        <template #append>
+          <VBtn
+            to="/menu/edit"
+            append-icon="mdi-chevron-right"
+            color="green"
+          >メニューを編集</VBtn>
+        </template>
+      </VListItem>
+      <VListItem
+        v-if="props.showPresetNav"
+        title="プリセット"
+        subtitle="どのメニューを行うか、一敗あたり何回行うかの設定"
+      >
+        <template #append>
+          <VBtn
+            to="/menu/preset"
+            append-icon="mdi-chevron-right"
+            color="green"
+          >プリセットを追加</VBtn>
+        </template>
+      </VListItem>
+    </VList>
+  </VCard>
+</template>
