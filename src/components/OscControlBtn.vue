5191c1d172b3c7306858f627a8b84d8fb89aa2f3
woorld <17719812+woorld@users.noreply.github.com>
2025-12-02 22:31:45 +0900

    OSCコントロールボタン OSCサーバー開始中に対象OSCメッセージを空文字列にした際、オフにできなくなるのを修正

--- src/components/OscControlBtn.vue
+++ src/components/OscControlBtn.vue
@@ -10,6 +10,7 @@ const isTargetOscMessageEmpty = ref(false);
 const label = computed(() => oscStore.isListening ? 'OSC受信中' : 'OSC受信停止中');
 const color = computed(() => oscStore.isListening ? 'green' : 'yellow');
 const icon = computed(() => oscStore.isListening ? 'mdi-wifi' : 'mdi-wifi-strength-off');
+const isTooltipVisible = computed(() => isTargetOscMessageEmpty.value && !oscStore.isListening);
 
 (async () => {
   const targetOscMessage = await window.setting.getSetting('targetOscMessage');
@@ -26,7 +27,7 @@ const icon = computed(() => oscStore.isListening ? 'mdi-wifi' : 'mdi-wifi-streng
     :color
     :prepend-icon="icon"
     :loading="oscStore.loading"
-    :disabled="oscStore.loading || isTargetOscMessageEmpty"
+    :disabled="oscStore.loading || isTooltipVisible"
     rounded
     @click="oscStore.toggleListeningStatus"
   >
@@ -36,7 +37,7 @@ const icon = computed(() => oscStore.isListening ? 'mdi-wifi' : 'mdi-wifi-streng
       location="left"
       no-click-animation
       interactive
-      :model-value="isTargetOscMessageEmpty"
+      :model-value="isTooltipVisible"
       :open-on-click="false"
       :open-on-focus="false"
       :open-on-hover="false"
