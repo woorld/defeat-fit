8809094222dcbea0153f53d3b60fd45ec4cf7bf9
woorld <17719812+woorld@users.noreply.github.com>
2025-12-08 01:50:44 +0900

    OSCストアに全メッセージ受信モードの操作を追加

--- src/stores/osc.ts
+++ src/stores/osc.ts
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 export const useOscStore = defineStore('osc', () => {
   const isListening = ref(false);
   const loading = ref(true);
+  const listenedMessageList = ref(new Set<string>());
 
   const toggleListeningStatus = async () => {
     loading.value = true;
@@ -19,7 +20,17 @@ export const useOscStore = defineStore('osc', () => {
     loading.value = false;
   };
 
+  const startListeningAll = async () => {
+    listenedMessageList.value.clear();
+    await window.osc.stopListening();
+    return window.osc.startListeningAll();
+  };
+
   (async () => {
+    window.osc.onListenAnyMessage((listenedMessage) => {
+      listenedMessageList.value.add(listenedMessage);
+    });
+
     await window.osc.startListening();
     isListening.value = await window.osc.getListeningStatus();
     loading.value = false;
@@ -28,6 +39,8 @@ export const useOscStore = defineStore('osc', () => {
   return {
     isListening,
     loading,
+    listenedMessageList,
     toggleListeningStatus,
+    startListeningAll,
   };
 });
