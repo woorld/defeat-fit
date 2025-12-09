9169523e43041bcc0bbb94d2caa87d6778197769
woorld <17719812+woorld@users.noreply.github.com>
2025-12-09 22:26:43 +0900

    基底ダイアログ 閉じれるか否かのオプションを追加

--- src/components/BaseDialog.vue
+++ src/components/BaseDialog.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const props = defineProps<{
   activateByParent?: boolean,
+  canClose?: boolean,
 }>();
 
 const emit = defineEmits<{
@@ -14,6 +15,7 @@ const model = defineModel<boolean>({ required: true });
   <VDialog
     v-model="model"
     :activator="props.activateByParent ? 'parent' : undefined"
+    :persistent="!canClose"
     @afterLeave="emit('close')"
   >
     <VSheet class="pa-8 text-center">
@@ -22,6 +24,7 @@ const model = defineModel<boolean>({ required: true });
     <VBtn
       class="position-absolute top-0 right-0 mt-2 mr-2 elevation-0"
       icon="mdi-close"
+      :disabled="!canClose"
       @click="model = false"
     />
   </VDialog>
