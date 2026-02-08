96eac1a7c3584db08b5f7544abfe36d3369cfcea
woorld <17719812+woorld@users.noreply.github.com>
2026-02-08 17:22:15 +0900

    設定API 設定ファイルのJSON構文が不正な場合の処理を追加

--- electron/api/setting.ts
+++ electron/api/setting.ts
@@ -1,12 +1,15 @@
 import Store from 'electron-store';
 import type { Setting, TargetOscMessageSetting } from '../../common/types';
 import { SETTING_DEFAULT_VALUE } from '../../common/constants';
-import { ipcMain, type IpcMainInvokeEvent } from 'electron';
+import { app, ipcMain, type IpcMainInvokeEvent } from 'electron';
 import { noticeApi } from './notice';
+import path from 'node:path';
+import fs from 'node:fs';
 
-const store = new Store<{ setting: Setting }>();
+type SettingStore = { setting: Setting };
+
+let store: Store<SettingStore> | null = null;
 const storeKey = 'setting';
-
 let isInitialized = false;
 
 const isTargetOscMessageSettingKey = (key: keyof Setting): key is 'targetOscMessage' =>
@@ -27,11 +30,55 @@ const formatTargetOscMessageSetting = (settings: TargetOscMessageSetting[]): Tar
   return validSettings;
 };
 
+const regenerateSettingFile = () => {
+  const userDataPath = app.getPath('userData');
+  const settingPath = path.join(userDataPath, 'config.json');
+  const renamedSettingPath = path.join(userDataPath, `config_bak-${Date.now()}.json`);
+
+  // JSONのパースに失敗した場合、既存の設定ファイルをリネームして新しい設定ファイルを生成する
+  try {
+    if (!fs.existsSync(settingPath)) {
+      throw Error('リネーム対象のファイルが存在しません');
+    }
+
+    fs.renameSync(settingPath, renamedSettingPath);
+
+    noticeApi.createNotice({
+      text: '破損した設定ファイルをバックアップし、設定ファイルを再作成しました',
+      color: 'success',
+    });
+  }
+  catch (e) {
+    console.error(e);
+    store = null;
+    noticeApi.createNotice({
+      text: '既存の設定ファイルのバックアップ・再作成処理に失敗しました',
+      color: 'error',
+    });
+  }
+};
+
+const getStore = (): Store<SettingStore> | null => {
+  try {
+    return new Store<SettingStore>();
+  }
+  catch (e) {
+    if (e instanceof SyntaxError) {
+      regenerateSettingFile();
+      return new Store<SettingStore>();
+    }
+    console.log(e);
+    return null;
+  }
+};
+
 export const settingApi = {
   initialize() {
     if (isInitialized) {
       return;
     }
+
+    store = getStore();
 
     ipcMain.handle('get-setting', (_, settingName: keyof Setting) => this.getSetting(settingName));
     ipcMain.handle('get-all-setting', () => this.getAllSetting());
@@ -55,7 +102,33 @@ export const settingApi = {
   },
 
   getAllSetting(): Setting {
-    return store.get(storeKey, SETTING_DEFAULT_VALUE);
+    if (store === null) {
+      store = getStore();
+      if (store === null) {
+        noticeApi.createNotice({
+          text: '設定の読み込みに失敗しました',
+          color: 'error',
+        });
+        return SETTING_DEFAULT_VALUE;
+      }
+    }
+
+    try {
+      return store.get(storeKey, SETTING_DEFAULT_VALUE);
+    }
+    catch (e) {
+      store = getStore();
+
+      if (store === null) {
+        noticeApi.createNotice({
+          text: '設定の読み込みに失敗しました',
+          color: 'error',
+        });
+        return SETTING_DEFAULT_VALUE;
+      }
+
+      return store.get(storeKey, SETTING_DEFAULT_VALUE);
+    }
   },
 
   setSetting<K extends keyof Setting>(settingName: K, value: Setting[K]) {
@@ -71,8 +144,36 @@ export const settingApi = {
   },
 
   setAllSetting(setting: Setting) {
+    if (store === null) {
+      store = getStore();
+      if (store === null) {
+        noticeApi.createNotice({
+          text: '設定の保存に失敗しました',
+          color: 'error',
+        });
+        return;
+      }
+    }
+
     setting.targetOscMessage = formatTargetOscMessageSetting(setting.targetOscMessage);
-    store.set(storeKey, setting);
+
+    try {
+      store.set(storeKey, setting);
+    }
+    catch (e) {
+      store = getStore();
+
+      if (store === null) {
+        noticeApi.createNotice({
+          text: '設定の保存に失敗しました',
+          color: 'error',
+        });
+        return;
+      }
+
+      store.set(storeKey, setting);
+    }
+
     noticeApi.createNotice({
       text: '設定を保存しました',
       color: 'success',
