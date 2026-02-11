f64ab631087bf246ee5eaee1eb0764addfec196f
woorld <17719812+woorld@users.noreply.github.com>
2026-02-11 21:40:26 +0900

    OSC受信時に設定に合わせた効果音が鳴る機能を追加

--- src/stores/defeat-count.ts
+++ src/stores/defeat-count.ts
@@ -1,5 +1,38 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
+import type { OscReceivedSoundSetting } from '../../common/types';
+import soundSlash from '../assets/sound/defeat/slash.mp3';
+import soundPull from '../assets/sound/defeat/pull.mp3';
+import soundExplode from '../assets/sound/defeat/explode.mp3';
+import soundBellSmall from '../assets/sound/defeat/bell-small.mp3';
+import soundBellLarge from '../assets/sound/defeat/bell-large.mp3';
+import soundShoot from '../assets/sound/defeat/shoot.mp3';
+import soundHit from '../assets/sound/defeat/hit.mp3';
+
+const oscReceivedSounds: Record<Exclude<OscReceivedSoundSetting, null>, string> = {
+  'slash': soundSlash,
+  'pull': soundPull,
+  'explode': soundExplode,
+  'bell-small': soundBellSmall,
+  'bell-large': soundBellLarge,
+  'shoot': soundShoot,
+  'hit': soundHit,
+};
+
+const playOscReceivedSound = async () => {
+  // TODO: 実行するたびに設定とってくるの何とかしたい OSCのストアと連携してOSCのリッスンが開始されたときに設定を取得して保持する？
+  const soundVariant = await window.setting.getSetting('oscReceivedSound');
+
+  if (soundVariant == null || !Object.keys(oscReceivedSounds).includes(soundVariant)) {
+    return;
+  }
+
+  const soundVolume = await window.setting.getSetting('soundVolume');
+  const audio = new Audio(oscReceivedSounds[soundVariant]);
+  audio.volume = soundVolume;
+  // audio.currentTime = 0;
+  audio.play();
+};
 
 export const useDefeatCountStore = defineStore('defeat-count', () => {
   const count = ref(0);
@@ -15,6 +48,9 @@ export const useDefeatCountStore = defineStore('defeat-count', () => {
   (async () => {
     getDefeatCount();
     window.defeatCount.onUpdateDefeatCount((newCount) => {
+      if (newCount > count.value) {
+        playOscReceivedSound();
+      }
       count.value = newCount;
     });
   })();
