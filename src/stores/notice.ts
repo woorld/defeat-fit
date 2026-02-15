36e64a805b53150bcb7178c39dbe657eeecedbdb
woorld <17719812+woorld@users.noreply.github.com>
2026-02-15 20:22:09 +0900

    src配下のcomposables, stores, 直下のファイルのimport文にエイリアスを適用

--- src/stores/notice.ts
+++ src/stores/notice.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { Notice } from '../../common/types';
+import { Notice } from '@common/types';
 import { ref } from 'vue';
 
 export const useNoticeStore = defineStore('notice', () => {
