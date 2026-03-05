3f05761f08de60984eb492dea6cdde53762eea99
woorld <17719812+woorld@users.noreply.github.com>
2026-03-05 18:34:32 +0900

    ビューのトグルボタンを共通化

--- src/views/timer-counter/TimerCounterView.vue
+++ src/views/timer-counter/TimerCounterView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ViewToggle from '@src/components/common/ViewToggle.vue';
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 
@@ -6,25 +7,24 @@ const route = useRoute();
 
 // NOTE: 現状/timer-counter以下にはtimer, counterしかないため、isInCounterViewは不要
 const isInTimerView = computed(() => route.path.startsWith('/timer-counter/timer'));
+
+const viewOptions = [
+  {
+    label: 'タイマー',
+    path: '/timer-counter/timer/0/1',
+    isActive: () => isInTimerView.value,
+  },
+  {
+    label: 'カウンター',
+    path: '/timer-counter/counter/0/1',
+    isActive: () => !isInTimerView.value,
+  },
+];
 </script>
 
 <template>
   <VContainer class="h-100">
-    <VBtnToggle
-      class="d-flex justify-center rounded-xl position-fixed left-0 right-0 mt-6"
-      density="comfortable"
-    >
-      <VBtn
-        border
-        to="/timer-counter/timer/0/1"
-        :active="isInTimerView"
-      >タイマー</VBtn>
-      <VBtn
-        border
-        to="/timer-counter/counter/0/1"
-        :active="!isInTimerView"
-      >カウンター</VBtn>
-    </VBtnToggle>
+    <ViewToggle class="position-fixed left-0 right-0" :viewOptions />
     <RouterView v-slot="{Component}">
       <Transition mode="out-in">
         <Component :is="Component" />
